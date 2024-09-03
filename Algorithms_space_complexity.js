53f9ab5e7c831878802085c2ab31280e9748b93e
Bence Nagy <nagybence0601@gmail.com>
2024-09-03 15:08:39 +0200

    Algorithms space complexity node added

---
+++ Algorithms_space_complexity.js
@@ -0,0 +1,40 @@
+// O(n) konstans tár komplexitás
+//fix számú változó használata
+
+function AddTwoNumbers(a,b){
+    return a+b
+}
+
+//O(n) Lineáris tár
+// A memóriahasználat lineárisan nő a bemenet méretével
+// Példa : N elemű tömb tárolása
+
+function CreateList(n){
+    return new Array(n).fill(0);
+}
+
+//O(n^2) Kvadratikus tár
+//A memóriahasználat négyzetesen nő a bemenet mértékével
+// Példa : 2D mátrix tárolása, ahol mindkét dimenzió 'n' arányos
+
+function CreateMatrix(n){
+    const matrix = [];
+    for(let i = 0;i<n;i++){
+        matrix.push(new Array(n).fill(0));
+    }
+    return matrix;
+}
+
+//O(2^n) Exponenciális tár
+// Példa : N méretű halmaz összes részhalmazának tárolása
+
+function GenerateSubSets(s){
+    const subsets = [[]];
+    for(const elem of s){
+        const newSubsets  = subsets.map(subset => subset.concat(elem))
+        subsets.push(...newSubsets)
+    }
+    return subsets
+}
+
+
--- Slow_and_fast_pointer.js
+++ Slow_and_fast_pointer.js
@@ -1,68 +1,30 @@
 class Node{
-    constructor(data,next = null){
+    constructor(data, next = null){
         this.data = data;
         this.next = next;
     }
 }
-const n1 = new Node(100);
 
-class LinkedList {
+class LinkedList{
     constructor(){
-        this.head = null;
+        this.head = null
         this.size = 0;
     }
-
     //insert first node
-    insertFirst(data){
-    this.head = new Node(data,this.head);
 
-
-}
     //insert last node
-    inserLast(data){
-        let node = new Node(data);
-        let current;
 
+    //insert at index
 
-        //if empty, make head
-        if(!this.head){
-            this.head = node;
-        }
-        else{
-            current = this.head;
-
-            while(current.next){
-                current = current.next;
-                
-            }
-        }
-
-
-        
-    }
-
-    //insert index
 
     //get an index
 
+
     //Remove at index
 
-    //print list data
-    printListData(){
-        let current = this.head;
-        while(current){
-            console.log(current.data);
-            current = current.next
-        }
-    }
+
+    //Clear list
+
+
+    //Print list data
 }
-
-const ll = new LinkedList();
-
-ll.insertFirst(100);
-ll.insertFirst(200);
-ll.insertFirst(300);
-ll.insertFirst(400);
-
-
-ll.printListData();
